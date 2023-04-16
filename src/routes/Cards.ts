a7af246940c10c759699e648db136283d7b673fb
bhanuteja958 <banu.teja958@gmail.com>
2023-04-16 22:38:11 +0530

    completed converting card apis to ts

--- src/routes/Cards.ts
+++ src/routes/Cards.ts
@@ -1,65 +1,59 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import {generateAPIResponse} from '../common/helper.js';
 import { createCardForAList, getCardDataOfList, updateCardOfList, deleteCardFromList, addMemberToCard, removeMemberFromCard } from '../controllers/Cards.js';
 
 const cardsRouter = express.Router();
 
-cardsRouter.post('/', async(req, res) => {
+cardsRouter.post('/', async(req:Request, res:Response) => {
     try {
-        const [status, response] = await createCardForAList(req);
+        const {status, response} = await createCardForAList(req);
         res.status(status).json(response);
     } catch(error) {    
-        console.log(error);
         res.status(500).json(generateAPIResponse('Something went wrong'));
     }
 });
 
-cardsRouter.get('/:card_id', async(req, res) => {
+cardsRouter.get('/:card_id', async(req:Request, res:Response) => {
     try {
-        const [status, response] = await getCardDataOfList(req);
+        const {status, response}= await getCardDataOfList(req);
         res.status(status).json(response);
     } catch(error) {
-        console.log(error);
         res.status(500).json(generateAPIResponse('Something went wrong'));
     }
 });
 
-cardsRouter.put('/:card_id', async(req, res) => {
+cardsRouter.put('/:card_id', async(req:Request, res:Response) => {
     try {
-        const [status, response] = await updateCardOfList(req);
+        const {status, response} = await updateCardOfList(req);
         res.status(status).json(response);
     } catch(error) {
-        console.log(error);
         res.status(500).json(generateAPIResponse('Something went wrong'));
     }
 });
 
-cardsRouter.delete('/:card_id', async(req, res) => {
+cardsRouter.delete('/:card_id', async(req:Request, res:Response) => {
     try {
-        const [status, response] = await deleteCardFromList(req);
+        const {status, response} = await deleteCardFromList(req);
         res.status(status).json(response);
     } catch(error) {
-        console.log(error);
         res.status(500).json(generateAPIResponse('Something went wrong'));
     }
 });
 
-cardsRouter.post('/addMember', async(req,res) => {
+cardsRouter.post('/addMember', async(req:Request, res:Response) => {
     try {
-        const [status, response] = await addMemberToCard(req);
+        const {status, response} = await addMemberToCard(req);
         res.status(status).json(response);
     } catch(error) {
-        console.log(error);
         res.status(500).json(generateAPIResponse('Something went wrong'));
     }
 });
 
-cardsRouter.delete('/removeMember/:card_id/:user_id', async(req, res) => {
+cardsRouter.delete('/removeMember/:card_id/:user_id', async(req:Request, res:Response) => {
     try {
-        const [status, response] = await removeMemberFromCard(req);
+        const {status, response} = await removeMemberFromCard(req);
         res.status(status).json(response);
     } catch(error) {
-        console.log(error);
         res.status(500).json(generateAPIResponse('Something went wrong'));
     }
 })
--- src/services/cards.ts
+++ src/services/cards.ts
@@ -1,191 +1,213 @@
+import { generateServiceResponse } from "../common/helper.js";
 import { Card } from "../models/Card.model.js";
 import { CardUserMapping } from "../models/CardUserMapping.model.js";
 import { List } from "../models/List.model.js";
 
-export const getBoardIdOfCard = async (card_id) => {
+export const getBoardIdOfCard = async (cardId: string) => {
     try {
         const getBoardIdOfCardResult = await Card.findOne({
             include: [
                 {model: List, required: true, attributes:['board_id']},
             ],
             where: {
-                id: card_id
+                id: cardId
             }   
         });
 
         if(!getBoardIdOfCardResult) {
-            return {
-                errorMessage: "No card with the given id exists",
-                status: 400,
-            }
+            return generateServiceResponse(
+                400,
+                true,
+                "No card with the given id exists"
+            )
         }
 
-        return getBoardIdOfCardResult;
+        return generateServiceResponse(
+            200,
+            false,
+            "Successfully fetched board id",
+            getBoardIdOfCardResult.dataValues.board_id
+        )
     } catch(error) {
-        console.log('Error while getting board id of card: ',error);
-        return {
-            errorMessage: 'Error while getting board id of card',
-            status: 400,
-        }
+        throw error;
     }
 }
 
-export const createCard = async (cardData, user_id) => {
+export const createCard = async (cardData:any, userId: string) => {
     try {
         const createCardResult = await Card.create({
             ...cardData,
-            created_by: user_id,
-            modified_by: user_id
+            created_by: userId,
+            modified_by: userId
         });
 
-        return createCardResult;
+        return generateServiceResponse(
+            200,
+            false,
+            "sucessfully created card"
+        )
     } catch(error) {
-        console.log('Error while creating card: ',error);
-        return {
-            errorMessage: 'Error while creating card',
-            status: 400,
-        }
+        throw error;
     }
 }
 
-export const getCard = async (card_id) => {
+export const getCard = async (cardId:string) => {
     try {
         const getCardResult = await Card.findOne({
             where: {
-                id: card_id,
+                id: cardId,
             }
-        })
+        });
 
-        return getCardResult;
-    } catch(error) {
-        console.log('Error while getiing card data:',error);
-        return {
-            errorMessage: 'Error while getting card data',
-            status: 400,
+        if(!getCardResult) {
+            return generateServiceResponse(
+                400,
+                true,
+                "No card with the given id exists",
+            )
         }
 
+        return generateServiceResponse(
+            200,
+            false,
+            'Successfully fetched card details',
+            getCardResult.dataValues
+        )
+    } catch(error) {
+       throw error;
     }
 }
 
-export const updateCard = async (card_id, cardValuesToBeUpdated, user_id) => {
+export const updateCard = async (cardId:string, cardValuesToBeUpdated:any, userId: string) => {
     try {
         const updateCardResult = await Card.update({
             ...cardValuesToBeUpdated,
-            modified_by: user_id
+            modified_by: userId
         }, {
             where: {
-                id: card_id
+                id: cardId
             }
         });
 
         if(updateCardResult[0] === 0) {
-            return {
-                errorMessage: 'No card with the given id exists',
-                status: 400
-            }
+            return generateServiceResponse(
+                400,
+                true,
+                'No card with the given id exists'
+            )
         }
 
-        return updateCardResult;
+        return generateServiceResponse(
+            200,
+            false,
+            "successfully updated card"
+        )
     } catch(error) {
-        console.log('Error while updating card: ',error);
-        return {
-            errorMessage: 'Error while updating card',
-            status: 400,
-        }
+        throw error
     }
 }
 
-export const deleteCard = async (card_id) => {
+export const deleteCard = async (cardId:string) => {
     try {
         const deleteCardResult = await Card.destroy({
             where: {
-                id: card_id
+                id: cardId
             }
         });
 
         if(deleteCardResult === 0) {
-            return {
-                errorMessage: 'No card with the given id exists',
-                status: 400
-            }
+            return generateServiceResponse(
+                400,
+                true,
+                'No card with the given id exists'
+            )
         }
-        return deleteCardResult;
+
+        return generateServiceResponse(
+            200,
+            false,
+            "Succesfully deleted card"
+        )
     } catch(error) {
-        console.log('Error while delete card: ', error);
-        return {
-            errorMessage: 'Error while delete card',
-            status: 400,
-        }
+       throw error;
     }
 }
 
-export const addMember = async (card_id, user_id, created_by) => {
+export const addMember = async (cardId:string, userId:string, createdBy:string) => {
     try {
         const addMemberResult = await CardUserMapping.create({
-            card_id,
-            user_id,
-            created_by
+            card_id: cardId,
+            user_id: userId,
+            created_by: createdBy
         });
 
-        return addMemberResult;
+        return generateServiceResponse(
+            200,
+            false,
+            "Successfully added memeber to card"
+        );
     } catch(error) {
-        console.log('Error while adding member to card: ', error);
-        return {
-            errorMessage: 'Error while adding member to card',
-            status: 400,
-        }
+        throw error;
     }
 }
 
-export const removeMember = async (card_id, user_id) => {
+export const removeMember = async (cardId: string, userId:string) => {
     try {
         const removeMemberResult = await CardUserMapping.destroy({
             where: {
-                card_id,
-                user_id,
+                card_id:cardId,
+                user_id:userId,
             }
         });
 
         if(removeMemberResult === 0) {
-            return {
-                errorMessage: 'No card with the given id exists',
-                status: 400
-            }
+            return generateServiceResponse(
+                400,
+                true,
+                'No card with the given id exists'
+            );
         }
 
-        return removeMemberResult;
+        return generateServiceResponse(
+            200,
+            false,
+            "Successfully removed member from card"
+        )
     } catch(error) {
-        console.log('Error while removing member from  card: ', error);
-        return {
-            errorMessage: 'Error while removing member from  card',
-            status: 400,
-        }
+       throw error;
     }
 }
 
-export const checkIfCardMember = async (card_id, user_id) => {
+export const checkIfCardMember = async (cardId:string, userId:string) => {
     try {
         const checkIfCardMemberResult = await CardUserMapping.findOne({
             attributes:['id'],
             where:{
-                card_id,
-                user_id
+                card_id:cardId,
+                user_id: userId
             }
         });
 
         if(checkIfCardMemberResult) {
-            return {
-                errorMessage: "User already is a member of card",
-                status: 400
-            }
+            return generateServiceResponse(
+                400,
+                true,
+                "User already is a member of card",
+                {
+                    isAlreadyMember: true
+                }
+            )
         }
 
-        return true
+        return generateServiceResponse(
+            200,
+            false,
+            "User is not a card member",
+            {
+                isAlreadyMember: false
+            }
+        )
     } catch(error) {
-        console.log('Error while checking member of card: ', error);
-        return {
-            errorMessage: 'Something went wrong',
-            status: 400,
-        }
+        throw error;
     }
 }
